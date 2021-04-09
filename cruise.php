2c182e026395a19005c292cd6179df041a7817c1
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-04-09 16:47:07 -0600

    added pictures to cruise

--- cruise.php
+++ cruise.php
@@ -2,6 +2,10 @@
 	<div id="mainwrapper">	
     <main>
 		<h2>Cruises</h2>
+		<img src="" alt="Australia"><h3>Austalia and New Zealand Cruises from $999</h3>
+		<h3>Alaskan Cruises from $999</h3>
+		<h3>European Cruises from $999</h3>
+		<h3>Carribean Cruises from $999</h3>
 	</main>	
 	</div>
 	<?php include '_footer.php';  ?> 
